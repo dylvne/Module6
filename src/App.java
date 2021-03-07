12e3022972711e04e4878c49b0abd2ac14a60070
Delane Chen <46573881+dylvne@users.noreply.github.com>
2021-03-06 22:27:47 -0500

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,137 @@
+
+//import java.io.Console;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import javax.swing.plaf.LabelUI;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    Stage window;
+    Scene scene1, scene2;
+
+    public static String fileName = "";
+    public static ArrayList<Word> wordList = new ArrayList<Word>();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+
+        //parts of the scene
+
+        // Button exit = new Button("Exit");
+        // exit.setOnAction(e -> System.exit(0));
+
+        window = primaryStage;
+
+        Label label1 = new Label("Welcome to the first scene!");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        //Layout 1 - children are laid out in a vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        //Button 2
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene (layout2, 600, 300);
+
+
+        primaryStage.setTitle("word occurrences");
+
+        primaryStage.setScene(scene1);
+
+        primaryStage.show();
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+        String text = "";
+        System.out.println(new File(".").getAbsoluteFile());
+        System.out.println("Please enter the name of the file to scan");
+        fileName = System.console().readLine();
+
+        File file = new File(fileName);
+
+        if (file.exists()){
+            System.out.println("this file exists");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                text = text + " " + scanner.nextLine();
+            }
+            scanner.close();
+            //System.out.println(text);
+            String[] wordArr = text.split("\\s+|-|—|\\.|\"|\\?|!|;|,|'s|\\r?\\n|:|\\s|'");
+            for (String string : wordArr) {
+                
+                //System.out.println(string);
+
+                if(string == ""|| string == " "){
+
+                }
+                else{
+                    if(inWordList(string,wordList)){
+                        increaseWordFreq(string, wordList);
+                    }
+                    else{
+                        wordList.add(new Word(string));
+                    }
+                }
+
+            }
+        }
+        else{
+            System.out.println("this file not found");
+        }
+
+        Collections.sort(wordList);
+
+        for (int i = 0; i < 20; i++) {
+            String string = wordList.get(i).getWord();
+            int frequency = wordList.get(i).getFrequency();
+            System.out.println(string + " : " + frequency);
+        }
+    }
+
+    public static boolean inWordList(String string, ArrayList<Word> wordList){
+
+        for (Word word : wordList) {
+            if(string.toLowerCase().equalsIgnoreCase(word.getWord())){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void increaseWordFreq(String string, ArrayList<Word> wordList){
+        for (Word word : wordList) {
+            if(string.toLowerCase().equalsIgnoreCase(word.getWord())){
+                word.increase();
+            }
+        }
+    }
+
+
+}
